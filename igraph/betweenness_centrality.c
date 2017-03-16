9ce0c5d128bb59c6aeb4a388a0ba6c8ca9096023
Dang Pham <dangph2011@gmail.com>
2017-03-17 02:00:01 +0700

    Change type fprintf int -> double

--- igraph/betweenness_centrality.c
+++ igraph/betweenness_centrality.c
@@ -15,12 +15,12 @@ double get_current_time_mlsec() {
 }
 
 /**
-    Print igraph_vector_t 
+    Print igraph_vector_t
 */
 void print_vector(igraph_vector_t *v) {
     long int i;
     for (i=0; i<igraph_vector_size(v); i++) {
-        fprintf(stdout, " %li\n", (long int) VECTOR(*v)[i]);
+        fprintf(stdout, " %lf\n", VECTOR(*v)[i]);
     }
     //fprintf(f, "\n");
 }
@@ -32,17 +32,17 @@ int main() {
     igraph_vector_t bet;
     FILE *f;
 
-    f=fopen("../data/facebook_combined.txt", "r");
+    f=fopen("../data/p2p-Gnutella31.txt", "r");
     igraph_read_graph_edgelist(&g, f, 0, 1);
 
     igraph_vector_init(&bet, 0);
-    igraph_betweenness(/*graph=*/ &g, /*res=*/ &bet, /*vids=*/ igraph_vss_all(), 
-             /*directed=*/0, /*weights=*/ 0, /*nobigint=*/ 1);
+    igraph_betweenness(/*graph=*/ &g, /*res=*/ &bet, /*vids=*/ igraph_vss_all(),
+             /*directed=*/1, /*weights=*/ NULL, /*nobigint=*/ 0);
 
     print_vector(&bet);
 
     fclose(f);
-    graph_vector_destroy(&bet);
+    //graph_vector_destroy(&bet);
     igraph_destroy(&g);
     return 0;
 }
